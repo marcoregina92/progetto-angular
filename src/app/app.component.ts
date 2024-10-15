4a2e545faaa81fec0802a3c3d4ff0cdf2c621b05
Marco <marcoregina92@gmail.com>
2024-10-15 18:02:51 +0200

    utilizzo di NGIF

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'progetto-angular';
 
+  isVisible = true
+
   onInput(e: Event) {
     this.title = ((<HTMLInputElement>e.target).value)
   }
